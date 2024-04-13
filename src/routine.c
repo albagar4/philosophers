e78fe290070512fee92b69d0a0d81de99200e99e
Alba <alba.gar.diaz@gmail.com>
2024-04-13 15:47:20 +0200

    changed last message

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: albagar4 <albagar4@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 14:19:59 by albagar4          #+#    #+#             */
-/*   Updated: 2024/04/13 14:57:41 by albagar4         ###   ########.fr       */
+/*   Updated: 2024/04/13 15:46:50 by albagar4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,9 @@ void	*ft_eat(t_philo *philos, t_param *table)
 	pthread_mutex_lock(&table->write);
 	print_action(philos, 3);
 	pthread_mutex_unlock(&table->write);
+	pthread_mutex_lock(&philos->table->mon_mutex);
+	philos->count--;
+	pthread_mutex_unlock(&philos->table->mon_mutex);
 	ft_usleep(philos, philos->table->time_to_eat);
 	pthread_mutex_unlock(&table->forks[lfork].mutex);
 	pthread_mutex_unlock(&table->forks[rfork].mutex);
@@ -37,9 +40,6 @@ void	*ft_sleep(t_philo *philos)
 {
 	pthread_mutex_lock(&philos->table->write);
 	print_action(philos, 2);
-	pthread_mutex_lock(&philos->table->mon_mutex);
-	philos->count--;
-	pthread_mutex_unlock(&philos->table->mon_mutex);
 	pthread_mutex_unlock(&philos->table->write);
 	ft_usleep(philos, philos->table->time_to_sleep);
 	return (NULL);
